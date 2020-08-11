b4af9c1472c961e0f39ce7a731c06056c7d6d499
BTKoke <68201226+BTKoke@users.noreply.github.com>
2020-08-11 14:13:41 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 17 21:11:45 2020
+
+@author: User
+"""
+
+from setuptools import setup
+
+setup(name='gym_qap',
+      version='0.0.1',
+      install_requires=['gym', 'numpy']#And any other dependencies required
+)
