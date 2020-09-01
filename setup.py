eda303e333fa7e5115f043cb239533bed302a6e2
BTKoke <68201226+BTKoke@users.noreply.github.com>
2020-09-01 16:33:37 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import find_packages, setu
 
 setup(name='gym_qap',
       version='0.0.1',
-      install_requires=['gym', 'numpy']#And any other dependencies required
+      install_requires=['gym', 'numpy'],
       packages=find_packages()
 )
