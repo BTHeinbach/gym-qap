636ea5a1f88ebc2c90acdb2eb7c2f3af838b92cf
BTKoke <68201226+BTKoke@users.noreply.github.com>
2020-09-01 17:03:52 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import find_packages, setup
 setup(name='gym_qap',
       version='0.0.1',
       install_requires=['gym', 'numpy'],
-      packages=find_packages()
+      packages=find_packages(),
       include_package_data=True
 )
